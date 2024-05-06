c7c9a275821d4f5496d276bc87a0aee4f2b04a80
Jorge A. Calderon <67562342+jfnxcode@users.noreply.github.com>
2024-05-06 09:39:13 -0400

    Update and rename index.php to prueba.php

---
+++ prueba.php
@@ -0,0 +1,203 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+</head>
+<body>
+    <h1><Bdo:l>CUENTAS BANCARIAS</Bdo:l></h1>
+
+    <div class="container">
+    <div>
+        <div class="container">
+            <img src="./img1.png" alt="" class="img-fluid" style="border-radius: 1rem 0 0 1rem;" width="1000" height="1000" >
+        </div>
+                    
+     <div class="card">
+        
+    <h3>Cuenta Corriente</h3>
+        <p>Una cuenta corriente bancaria es un tipo de cuenta que permite a los individuos y empresas realizar transacciones financieras, como depósitos, retiros y pagos, de manera conveniente y segura.</p>
+        <p>Algunas características de una cuenta corriente incluyen:</p>
+        <ul>
+            <li>Acceso a cheques para realizar pagos y transferencias.</li>
+            <li>Posibilidad de realizar depósitos y retiros en sucursales bancarias.</li>
+            <li>Acceso a servicios en línea para realizar transacciones electrónicas.</li>
+            <li>Posibilidad de establecer límites de sobregiro para cubrir gastos inesperados.</li>
+            <li>Registro detallado de transacciones para facilitar el seguimiento de los movimientos de dinero.</li>
+        </ul>
+        <p>Registrate y Crea tu cuenta haciendo CLICK aqui:</p>
+        <button onclick="location='//localhost:8080/Examen324/index.php'">Click</button>
+    </div>
+</div>
+
+<div>
+
+<div class="container">
+            <img src="./img2.png" alt="" class="img-fluid" style="border-radius: 1rem 0 0 1rem;" width="1000" height="1000" >
+        </div>
+    <div class="card">
+        
+        <h3>Cuenta de Ahorros (Caja de Ahorros)</h3>
+        <p>Una cuenta de ahorros bancaria es un tipo de cuenta que permite a las personas guardar dinero y ganar intereses sobre el saldo depositado.</p>
+        <p>Algunas características de una cuenta de ahorros incluyen:</p>
+        <ul>
+            <li>Intereses que se acumulan sobre el saldo.</li>
+            <li>Acceso a servicios en línea para realizar seguimiento de ahorros y transacciones.</li>
+            <li>Posibilidad de establecer metas de ahorro y automatizar depósitos periódicos.</li>
+            <li>Flexibilidad para retirar fondos cuando sea necesario, aunque pueden aplicarse límites a la cantidad de retiros.</li>
+            <li>Seguridad proporcionada por el banco para proteger los ahorros.</li>
+        </ul>
+        <p>Registrate y Crea tu cuenta haciendo CLICK aqui:</p>
+        <button onclick="location= '//localhost:8080/Examen324/index.php'">Click</button>
+    </div>
+</div>
+
+
+<div>
+    <div class="container">
+            <img src="./img3.jpeg" alt="" class="img-fluid" style="border-radius: 1rem 0 0 1rem;" width="1000" height="1000" >
+        </div>
+    <div class="card">
+        
+        <h3>Cuenta de Cheques</h3>
+        <p>Una cuenta de cheques bancaria es un tipo de cuenta que permite a las personas realizar transacciones financieras diarias, como depósitos, retiros y pagos, mediante el uso de cheques o tarjetas de débito.</p>
+        <p>Algunas características de una cuenta de cheques incluyen:</p>
+        <ul>
+            <li>Acceso a cheques para realizar pagos y transferencias.</li>
+            <li>Posibilidad de realizar depósitos y retiros en sucursales bancarias.</li>
+            <li>Acceso a servicios en línea para realizar seguimiento de transacciones y saldos.</li>
+            <li>Posibilidad de establecer límites de gasto para proteger contra sobregiros.</li>
+            <li>Flexibilidad para realizar pagos en línea y a través de dispositivos móviles.</li>
+        </ul>
+        <p>Registrate y Crea tu cuenta haciendo CLICK aqui:</p>
+        <button onclick="location= '//localhost:8080/Examen324/index.php'">Click</button>
+    </div>
+</div>
+
+
+
+<div>
+
+<div class="container">
+            <img src="./img4.jpeg" alt="" class="img-fluid" style="border-radius: 1rem 0 0 1rem;" width="1000" height="1000" >
+        </div>
+    <div class="card">
+        
+        <h3>Cuenta de Inversión</h3>
+        <p>Una cuenta de inversión es un tipo de cuenta que permite a las personas invertir su dinero en diferentes instrumentos financieros con el objetivo de obtener rendimientos.</p>
+        <p>Algunas características de una cuenta de inversión incluyen:</p>
+        <ul>
+            <li>Acceso a una variedad de opciones de inversión, como acciones, bonos, fondos mutuos, y más.</li>
+            <li>Posibilidad de diversificar la cartera de inversión para reducir el riesgo.</li>
+            <li>Acceso a herramientas y recursos para realizar análisis de inversiones y tomar decisiones informadas.</li>
+            <li>Posibilidad de obtener ingresos pasivos a través de dividendos, intereses u otros rendimientos de inversión.</li>
+            <li>Flexibilidad para comprar y vender inversiones según las condiciones del mercado y los objetivos personales.</li>
+        </ul>
+        <p>Registrate y Crea tu cuenta haciendo CLICK aqui:</p>
+        <button onclick="location='//localhost:8080/Examen324/index.php'">Click</button>
+    </div>
+
+</div>
+
+
+</div>
+</body>
+</html>
+
+<style>
+    body {
+        font-family: Arial, sans-serif;
+        margin: 0;
+        padding: 0;
+        background-color: #f2f2f2;
+    }
+
+    h1 {
+        text-align: center;
+        margin-top: 50px;
+        color: #333;
+    }
+
+    div {
+        margin: 5px;
+        padding: 5px;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+        background-color: #fff;
+        box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+    }
+
+    img {
+        width: 200px;
+        height:200px;
+        margin-right: 10px;
+        float: left;
+    }
+
+    h3 {
+        margin-top: 0;
+        color: #333;
+    }
+
+    p {
+        margin-bottom: 10px;
+        color: #666;
+    }
+
+    button {
+        padding: 10px 20px;
+        background-color: #4CAF50;
+        color: white;
+        border: none;
+        border-radius: 5px;
+        cursor: pointer;
+        transition: background-color 0.3s ease;
+    }
+
+    button:hover {
+        background-color: #45a049;
+    }
+    .container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-wrap: wrap;
+    }
+    .card {
+        width: 300px;
+        margin: 20px;
+        padding: 20px;
+        border-radius: 10px;
+        box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+        background-color: #fff;
+        text-align: center;
+    }
+    .card img {
+        width: 100px;
+        height: 100px;
+        margin-bottom: 10px;
+    }
+    .card h3 {
+        margin: 0;
+        color: #333;
+    }
+    .card p {
+        margin-bottom: 20px;
+        color: #666;
+    }
+    .card button {
+        padding: 10px 20px;
+        background-color: #4CAF50;
+        color: white;
+        border: none;
+        border-radius: 5px;
+        cursor: pointer;
+        transition: background-color 0.3s ease;
+    }
+    .card button:hover {
+        background-color: #45a049;
+    }
+</style>
